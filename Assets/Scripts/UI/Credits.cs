bbfbc0981ab6cee5aaad27a51f767e6d276f0b27
CaptWulfz <jesinmartinez21@gmail.com>
2022-01-29 23:18:51 +0800

    Updated Master Scene with Credits Finished Game Manager Functionality

---
+++ Assets/Scripts/UI/Credits.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField] NoDragScrollRect scrollRect;
+    [SerializeField] Button playAgainButton;
+
+    private float yPos = 1f;
+    private float speed;
+    private bool startCredits = false;
+
+    private void OnEnable()
+    {
+        this.yPos = 1f;
+        this.speed = 0.05f;
+        this.playAgainButton.gameObject.SetActive(false);
+        StartCoroutine(DelayCredits());
+    }
+
+    private void Update()
+    {
+        if (this.startCredits)
+        {
+            this.yPos -= speed * Time.deltaTime;
+            Debug.Log("YPos: " + yPos);
+            this.scrollRect.normalizedPosition = new Vector2(0, yPos);
+        }
+
+        if (yPos < 0)
+        {
+            this.startCredits = false;
+            StartCoroutine(DelayShowPlayAgain());
+        }
+    }
+
+    private IEnumerator DelayCredits()
+    {
+        yield return new WaitForSeconds(2f);
+        this.startCredits = true;
+    }
+
+    private IEnumerator DelayShowPlayAgain()
+    {
+        yield return new WaitForSeconds(2f);
+        ShowPlayAgainButton();
+    }
+
+    private void ShowPlayAgainButton()
+    {
+        this.playAgainButton.gameObject.SetActive(true);
+    }
+
+    #region Unity Button Events
+    public void PlayAgainButtonClicked()
+    {
+        GameManager.Instance.InitializeGame(1);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/UI/NoDragScrollRectEditor.cs
@@ -0,0 +1,5 @@
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(NoDragScrollRect))]
+public class NoDragScrollRectEditor : ScrollRectEditor { }
